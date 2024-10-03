2a0a3f2b03b29a3b07f3844da5a30891158af4b8
Florent Poujol <florentpoujol@fastmail.com>
2024-10-03 12:21:41 +0200

    setup pint and phpstan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,21 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+final class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        UserFactory::new()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
