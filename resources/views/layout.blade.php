467061dba4c433e1a87bc2d926126e21230337fc
Florent Poujol <florentpoujol@fastmail.com>
2024-10-07 10:28:55 +0200

    Add a global navigation bar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,21 @@
         </style>
     </head>
     <body>
+        <nav>
+            <ul>
+                <li><a href="{{ route('blog') }}">blog</a></li>
+
+                @guest()
+                <li><a href="{{ route('login.show') }}">login</a></li>
+                @endguest
+
+                @auth()
+                <li><a href="{{ route('profile.show') }}">profile</a></li>
+                <li><a href="{{ route('logout') }}">logout</a></li>
+                @endauth
+            </ul>
+        </nav>
+
         @section('body')
 
         @show
